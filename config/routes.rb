899110fa791d3e04d6409deb509a67f87071243d
ToshikiKobayashi <software.engineer.0612@gmail.com>
2020-05-22 00:16:19 +0900

    Modified Login Viewer

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root   'photo_images#index'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  
+  resources :photo_images
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
